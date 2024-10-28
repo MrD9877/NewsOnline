a9789fed0f1a8952023606ee1b80e25653ed6c04
Dhuruv Bansal <Dhuruvbansal53@gmail.com>
2024-10-28 14:21:09 +0530

    api change

--- src/App.js
+++ src/App.js
@@ -5,26 +5,26 @@ import LoadingBar from 'react-top-loading-bar'
 
 
 export class App extends Component {
-  constructor(){
+  constructor() {
     super()
-    this.state={
-     progress:10,
-     apiUrl:"https://newsapi.org/v2/everything?q=apple&from=2024-09-16&to=2024-09-16&sortBy=popularity&apiKey=f176f5d05ab5455b9be2595fe143d8fe"
+    this.state = {
+      progress: 10,
+      apiUrl: "https://newsapi.org/v2/everything?q=apple&from=2024-09-16&to=2024-09-16&sortBy=popularity&apiKey=f176f5d05ab5455b9be2595fe143d8fe"
     }
   }
-  setProgress=(progress)=>{
-    this.setState({progress:progress})
+  setProgress = (progress) => {
+    this.setState({ progress: progress })
   }
- 
+
   render() {
     return (
       <>
-      <Navbar/>
-      <LoadingBar
-        color='#f11946'
-        progress={this.state.progress}
-      />
-      <News pageSize={6} setProgress={this.setProgress} apiUrl={this.state.apiUrl} testing={999}/>
+        <Navbar />
+        <LoadingBar
+          color='#f11946'
+          progress={this.state.progress}
+        />
+        <News pageSize={6} setProgress={this.setProgress} apiUrl={this.state.apiUrl} testing={999} />
       </>
     )
   }
